b9c1a8f6a7d4095200412598cc38073aca2f0a51
dl <liuchangyu1111@gmail.com>
2024-02-29 21:19:11 +0800

    upgrade

--- reality_sr/engine/trainer.py
+++ reality_sr/engine/trainer.py
@@ -272,18 +272,24 @@ class Trainer:
                                  out_channels=self.model_config_dict.G.get("OUT_CHANNELS", 3),
                                  channels=self.model_config_dict.G.get("CHANNELS", 64),
                                  num_rcb=self.model_config_dict.G.get("NUM_RCB", 16))
-        elif model_g_type == "edsrnet_x8":
-            g_model = edsrnet_x8(in_channels=self.model_config_dict.G.get("IN_CHANNELS", 3),
+        elif model_g_type == "rrdbnet_x2":
+            g_model = rrdbnet_x2(in_channels=self.model_config_dict.G.get("IN_CHANNELS", 3),
                                  out_channels=self.model_config_dict.G.get("OUT_CHANNELS", 3),
                                  channels=self.model_config_dict.G.get("CHANNELS", 64),
-                                 num_rcb=self.model_config_dict.G.get("NUM_RCB", 16))
+                                 growth_channels=self.model_config_dict.G.get("GROWTH_CHANNELS", 32),
+                                 num_rrdb=self.model_config_dict.G.get("NUM_RRDB", 23))
+        elif model_g_type == "rrdbnet_x3":
+            g_model = rrdbnet_x3(in_channels=self.model_config_dict.G.get("IN_CHANNELS", 3),
+                                 out_channels=self.model_config_dict.G.get("OUT_CHANNELS", 3),
+                                 channels=self.model_config_dict.G.get("CHANNELS", 64),
+                                 growth_channels=self.model_config_dict.G.get("GROWTH_CHANNELS", 32),
+                                 num_rrdb=self.model_config_dict.G.get("NUM_RRDB", 23))
         elif model_g_type == "rrdbnet_x4":
             g_model = rrdbnet_x4(in_channels=self.model_config_dict.G.get("IN_CHANNELS", 3),
                                  out_channels=self.model_config_dict.G.get("OUT_CHANNELS", 3),
                                  channels=self.model_config_dict.G.get("CHANNELS", 64),
                                  growth_channels=self.model_config_dict.G.get("GROWTH_CHANNELS", 32),
                                  num_rrdb=self.model_config_dict.G.get("NUM_RRDB", 23))
-
         else:
             raise NotImplementedError(f"Model type `{model_g_type}` is not implemented.")
         g_model = g_model.to(self.device)
@@ -474,11 +480,11 @@ class Trainer:
             sinc_kernel = sic_kernel.to(device=self.device, non_blocking=True)
             loss_pixel_weight = torch.Tensor(self.pixel_loss_weight).to(device=self.device)
 
-            # Initialize the generator gradient
-            self.g_model.zero_grad()
-
             # degradation transforms
             gt_usm, gt, lr = self.degradation_transforms(gt, gaussian_kernel1, gaussian_kernel2, sinc_kernel)
+
+            # Initialize the generator gradient
+            self.g_model.zero_grad()
 
             # Mixed precision training
             with amp.autocast(enabled=self.device.type != "cpu"):
@@ -629,7 +635,6 @@ class Trainer:
         self.g_model.train()
         # if self.rank != -1:
         #     self.train_dataloader.sampler.set_epoch(self.current_epoch)
-        self.g_optimizer.zero_grad()
 
         # The information printed by the progress bar
         self.batch_time = AverageMeter("Time", ":6.3f")
@@ -641,7 +646,6 @@ class Trainer:
 
         if self.phase == "gan":
             self.d_model.train()
-            self.d_optimizer.zero_grad()
             self.feature_losses = AverageMeter("Feature loss", ":.4e")
             self.adv_losses = AverageMeter("Adv loss", ":.4e")
             self.d_gt_probes = AverageMeter("D(GT)", ":6.3f")
@@ -667,7 +671,11 @@ class Trainer:
         self.eval_model()
 
         # save g ckpt
-        is_best = self.psnr > self.best_psnr or self.ssim > self.best_ssim
+        if self.phase == "gan":
+            is_best = self.niqe < self.best_niqe
+        else:
+            is_best = self.psnr > self.best_psnr or self.ssim > self.best_ssim
+
         ckpt = {
             "model": deepcopy(self.g_model).half(),
             "ema": deepcopy(self.ema.ema).half(),
@@ -684,7 +692,6 @@ class Trainer:
             self.d_lr_scheduler.step()
 
             # save d ckpt
-            is_best = self.niqe < self.best_niqe
             ckpt = {
                 "model": self.d_model,
                 "ema": None,
