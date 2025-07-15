f442d83d0ffed2f5a78207c0ef175d325fe88e2c
Lornatang <liuchangyu1111@gmail.com>
2025-07-15 21:50:19 +0800

    feat: enhance multi-scale dataset generation with validation and logging

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # reality-sr
 Real world super resolution library
+
+## Train
+
+### Introduction
+
+We follow the standard GAN training process, which is a two-stage training. The first stage trains the generative model based on PSNR loss, and the second stage trains the generative model and adversarial model based on PSNR loss, perceptual loss and GAN loss.
+
+### Data Preparation
+
+Usually, we recommend using the df2k dataset, which is a combined dataset consisting of [DIV2K](https://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_HR.zip)+[Flickr2K](https://cv.snu.ac.kr/research/EDSR/Flickr2K.tar).
+
+Since we use the degradation method, only the GT(Ground-Truth) image is needed.
+
+#### 1- Download df2k dataset
+
+```bash
+mkdir -p ./datasets/df2k
+wget https://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_HR.zip -O ./datasets/DIV2K_train_HR.zip
+wget https://cv.snu.ac.kr/research/EDSR/Flickr2K.tar -O ./datasets/Flickr2K.tar
+unzip ./datasets/DIV2K_train_HR.zip -d ./datasets
+tar -xf ./datasets/Flickr2K.tar -C ./datasets
+```
+
+#### 2- Multi-scale image generation
+
+Multi-scale image training helps the model learn features at different scales, thereby showing better generalization in real-world scenarios.
+
+```bash
+python3 ./tools/misc/generate_multi_scale_dataset.py -i ./datasets/DIV2K_train_HR -o ./datasets/df2k
+python3 ./tools/misc/generate_multi_scale_dataset.py -i ./datasets/Flickr2K -o ./datasets/df2k
+```
+
+#### 3- Combine datasets
+
+Fusion of multi-scale datasets and original datasets to further improve model generalization.
+
+```bash
+mv ./datasets/DIV2K_train_HR/* ./datasets/df2k
+mv ./datasets/Flickr2K/* ./datasets/df2k
+```
--- tools/misc/generate_multi_scale_dataset.py
+++ tools/misc/generate_multi_scale_dataset.py
@@ -12,77 +12,156 @@
 # limitations under the License.
 # ==============================================================================
 import argparse
-from abc import ABC
+import logging
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from pathlib import Path
+from typing import List, Tuple, Union
 
 import cv2
-from torchvision.datasets.folder import IMG_EXTENSIONS
+from tqdm import tqdm
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s.%(msecs)03d - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+LOGGER = logging.getLogger(__name__)
+
+IMAGE_SUFFIXES = [".bmp", ".dng", ".jpeg", ".jpg", ".mpo", ".png", ".tif", ".tiff", ".webp", ".pfm", ".heic"]
 
 
 def get_opts() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="Split image to sub images.")
+    parser = argparse.ArgumentParser()
     parser.add_argument(
         "-i", "--inputs",
-        type=Path,
-        default="./images",
-        help="Path to input image directory. Default: ``./images``",
+        default="./datasets/df2k",
+        type=str,
+        help="Path to input image directory. Defaults to ``./datasets/df2k``.",
     )
     parser.add_argument(
         "-o", "--output",
-        type=Path,
-        default="./multi_scale_images",
-        help="Path to output multi-scale image directory. Default: ``./multi_scale_images``",
+        default="./datasets/df2k_multi_scale",
+        type=str,
+        help="Path to output multi-scale image directory. Defaults to ``./datasets/df2k_multi_scale``.",
     )
     parser.add_argument(
         "--small-size",
-        type=int,
         default=384,
-        help="Small size of sliding window. Default: 384")
+        type=int,
+        help="Small size of sliding window. Defaults to 384.")
+    parser.add_argument(
+        "--scale-list",
+        nargs="+",
+        default=[0.33, 0.5, 0.75],
+        type=float,
+        help="List of scales to generate multi-scale images. Defaults to [0.33, 0.5, 0.75].",
+    )
+    parser.add_argument(
+        "-s", "--skip",
+        action="store_true",
+        help="Skip check and conversion for images without corresponding labels.",
+    )
     return parser.parse_args()
 
 
-class GenerateMultiScaleDataset(ABC):
-    def __init__(self, opts: argparse.Namespace) -> None:
-        super().__init__()
-        self.inputs = opts.inputs
-        self.output = opts.output
-        self.small_size = opts.small_size
-        self.scale_list = [0.75, 0.5, 1 / 3]
+def validate_image(image_dir: Union[Path, str], skip: bool = False) -> Tuple[List[str], List[str]]:
+    image_dir = Path(image_dir)
 
-    def run(self):
-        all_image_file_paths = [p for p in self.inputs.glob("*")]
+    valid_image_path_list, invalid_image_path_list = [], []
 
-        num_images = len(all_image_file_paths)
-        if num_images == 0:
-            raise ValueError(f"Directory '{self.inputs}' not have supported image files. Supported image formats: {IMG_EXTENSIONS}")
+    image_path_list = [file for file in image_dir.rglob("*") if file.suffix.lower() in IMAGE_SUFFIXES]
 
-        self.output.mkdir(parents=True, exist_ok=True)
+    for image_path in image_path_list:
+        if not skip:
+            image = cv2.imread(str(image_path))
+            if image is None:
+                invalid_image_path_list.append(str(image_path.absolute()))
+                continue
 
-        for image_file_path in all_image_file_paths:
-            image = cv2.imread(str(image_file_path), cv2.IMREAD_UNCHANGED)
-            height, width = image.shape[:2]
-            if height < self.small_size or width < self.small_size:
-                # save the smallest image
-                if width < height:
-                    ratio = height / width
-                    width = self.small_size
-                    height = int(width * ratio)
-                else:
-                    ratio = width / height
-                    height = self.small_size
-                    width = int(height * ratio)
-                new_image = cv2.resize(image, (width, height), interpolation=cv2.INTER_LANCZOS4)
-                new_image_file_path = Path(self.output, f"{image_file_path.stem}-scale{len(self.scale_list) + 1}{image_file_path.suffix}")
-                cv2.imwrite(str(new_image_file_path), new_image)
+        valid_image_path_list.append(str(image_path.absolute()))
 
-            for index, scale in enumerate(self.scale_list):
-                new_image = cv2.resize(image, (int(width * scale), int(height * scale)), interpolation=cv2.INTER_LANCZOS4)
-                new_image_file_path = Path(self.output, f"{image_file_path.stem}-scale{index}{image_file_path.suffix}")
-                cv2.imwrite(str(new_image_file_path), new_image)
+    return valid_image_path_list, invalid_image_path_list
+
+
+def _generate_multi_scale_dataset(
+        image_path: Union[Path, str],
+        output_dir: Union[Path, str],
+        small_size: int,
+        scale_list: List[float],
+) -> None:
+    image_path = Path(image_path)
+    output_dir = Path(output_dir)
+
+    try:
+        image = cv2.imread(str(image_path), cv2.IMREAD_UNCHANGED)
+        if image is None:
+            LOGGER.warning(f"Reading image failed: {image_path}. Skipping.")
+            return
+
+        height, width = image.shape[:2]
+
+        if height < small_size or width < small_size:
+            if width < height:
+                ratio = height / width
+                width = small_size
+                height = int(width * ratio)
+            else:
+                ratio = width / height
+                height = small_size
+                width = int(height * ratio)
+            new_image = cv2.resize(image, (width, height), interpolation=cv2.INTER_LANCZOS4)
+            new_image_path = Path(output_dir, f"{image_path.stem}-scale{len(scale_list) + 1}{image_path.suffix}")
+            cv2.imwrite(str(new_image_path), new_image)
+
+        for index, scale in enumerate(scale_list):
+            new_image = cv2.resize(image, (int(width * scale), int(height * scale)), interpolation=cv2.INTER_LANCZOS4)
+            new_image_path = Path(output_dir, f"{image_path.stem}-scale{index}{image_path.suffix}")
+            cv2.imwrite(str(new_image_path), new_image)
+
+        LOGGER.debug(f"Processed: {image_path.name}.")
+
+    except Exception as e:
+        LOGGER.error(f"Error processing {image_path}: {e}.")
+
+
+def generate_multi_scale_dataset(
+        inputs_dir: Union[Path, str],
+        output_dir: Union[Path, str],
+        small_size: int = 384,
+        scale_list: List[float] = None,
+        skip: bool = False,
+) -> None:
+    inputs_dir = Path(inputs_dir)
+    output_dir = Path(output_dir)
+
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    if scale_list is None:
+        scale_list = [0.33, 0.5, 0.75]
+
+    valid_image_path_list, invalid_image_path_list = validate_image(inputs_dir, skip)
+    if not valid_image_path_list:
+        LOGGER.error("No valid image files found.")
+        return
+    if invalid_image_path_list:
+        LOGGER.warning(f"Invalid or corrupt images: {invalid_image_path_list}")
+
+    with ThreadPoolExecutor() as executor:
+        tasks = [
+            executor.submit(
+                _generate_multi_scale_dataset,
+                valid_image_path,
+                output_dir,
+                small_size,
+                scale_list,
+            ) for valid_image_path in valid_image_path_list
+        ]
+        with tqdm(total=len(tasks), desc="Generate multi scale dataset", unit="file") as pbar:
+            for _ in as_completed(tasks):
+                pbar.update(1)
+
+
+def main() -> None:
+    opts = get_opts()
+
+    generate_multi_scale_dataset(opts.inputs, opts.output, opts.small_size, opts.scale_list, opts.skip)
 
 
 if __name__ == "__main__":
-    opts = get_opts()
-
-    app = GenerateMultiScaleDataset(opts)
-    app.run()
+    main()
